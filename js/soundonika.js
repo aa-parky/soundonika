1388631f8f4b716dad5ad11e04835051678fd64a
aapark <aa.parky@gmail.com>
2025-09-06 08:35:44 +0100

    selectable wav files

--- js/soundonika.js
+++ js/soundonika.js
@@ -1,13 +1,32 @@
 class SoundonikaEngine {
-    constructor() {
+    constructor(audioContext) {
+        this.audioContext = audioContext;  // NEW
         this.sampleIndex = {};
         this.sampleBasePath = '../samples';
         this.samples = new Map();
+        this.volume = 0.8;
+        this.mode = 'samples';
     }
 
     async loadSampleIndex() {
         const response = await fetch(`${this.sampleBasePath}/sample-index.json`);
         this.sampleIndex = await response.json();
+    }
+
+    setVolume(vol) {
+        this.volume = vol;
+    }
+
+    getVolume() {
+        return this.volume;
+    }
+
+    setSoundMode(mode) {
+        this.mode = mode;
+    }
+
+    getSoundMode() {
+        return this.mode;
     }
 
     getAvailableSampleCategories() {
@@ -28,9 +47,21 @@ class SoundonikaEngine {
     previewSample(category, pack, filename) {
         const url = `${this.sampleBasePath}/${category}/${pack}/${filename}`;
         const audio = new Audio(url);
-        audio.volume = 0.8;
+        audio.volume = this.volume;
         audio.play().then(() => {
-            // Audio playback started successfully
+            // success
         });
+    }
+
+    playClickSound(name) {
+        const osc = this.audioContext.createOscillator();  // Use shared context
+        const gain = this.audioContext.createGain();
+
+        osc.frequency.value = name === 'kick' ? 100 : 300;
+        gain.gain.value = this.volume;
+
+        osc.connect(gain).connect(this.audioContext.destination);
+        osc.start();
+        osc.stop(this.audioContext.currentTime + 0.1);
     }
 }
